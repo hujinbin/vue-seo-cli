bcdcbabb35fb9270144802e03cab2c4cbcc48e40
hujinbin <437044048@qq.com>
2021-05-16 21:10:53 +0800

    fix: seo输出配置修改

--- build/webpack.seo.conf.js
+++ build/webpack.seo.conf.js
@@ -117,7 +117,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     
     new PrerenderSpaPlugin(
       // 编译后的html需要存放的路径
-      path.join(__dirname, '../dist'),
+      path.join(__dirname, '../seo'),
       // 列出哪些路由需要预渲染
       [ '/', '/about', '/contact' ]
     ),
